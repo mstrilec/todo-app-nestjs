e105f8eb646b39ce4c96b46c4ad9d022d9c661eb
mstrilec <MaxStrilec2005@gmail.com>
2023-10-09 14:52:10 +0300

    fix problem with db and create migration

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,24 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const createDataSourceOptions = async (
-  configService: ConfigService,
-): Promise<DataSourceOptions> => {
-  return {
-    type: 'mysql',
-    host: configService.get('DB_HOST'),
-    port: +configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    migrations: ['dist/db/migrations/*.{js,ts}'],
-    synchronize: false,
-  };
-};
+require('dotenv').config();
 
 const configService = new ConfigService();
-const dataSourceOptions = await createDataSourceOptions(configService);
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: +configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.{js,ts}'],
+  synchronize: false,
+};
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
