c759ddade26821ad624de03423e4a2465c72ebdb
mstrilec <MaxStrilec2005@gmail.com>
2023-10-09 14:52:10 +0300

    fix problem with db and create migration

---
+++ db/migrations/1696844422166-NewMigration.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class NewMigration1696844422166 implements MigrationInterface {
+    name = 'NewMigration1696844422166'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` (\`username\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`todo\` (\`id\` int NOT NULL AUTO_INCREMENT, \`title\` varchar(255) NOT NULL, \`isCompleted\` tinyint NOT NULL DEFAULT 0, \`userId\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`todo\` ADD CONSTRAINT \`FK_1e982e43f63a98ad9918a86035c\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`todo\` DROP FOREIGN KEY \`FK_1e982e43f63a98ad9918a86035c\``);
+        await queryRunner.query(`DROP TABLE \`todo\``);
+        await queryRunner.query(`DROP INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` ON \`user\``);
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { validationSchema } from './validation/validation.schema';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
-import { createDataSourceOptions } from 'db/data-source';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const dataSourceOptions = await createDataSourceOptions(configService);
-        return dataSourceOptions;
-      },
-    }),
-    UserModule,
+    TypeOrmModule.forRoot(dataSourceOptions),
     TodoModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
