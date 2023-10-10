3e7c260acee9f22b1c4267f0f4e57ff15ea51e74
mstrilec <MaxStrilec2005@gmail.com>
2023-10-10 12:23:07 +0300

    start implementing jwt auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,29 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+
+export type UserType = any;
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private usersRepository) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  private readonly users = [
+    {
+      id: 1,
+      username: 'john',
+      password: 'changeme',
+      todos: [],
+    },
+    {
+      id: 2,
+      username: 'maria',
+      password: 'guess',
+      todos: [],
+    },
+  ];
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
   }
 }
