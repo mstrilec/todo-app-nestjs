e49d3ecb270411ac168b4ff1157ac6f53346115f
mstrilec <MaxStrilec2005@gmail.com>
2023-10-13 13:03:51 +0300

    Completed JWT Authentication Implementation

--- src/auth/local-strategy.ts
+++ src/auth/local-strategy.ts
@@ -14,7 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     const user = await this.authService.validateUser(username, password);
 
     if (!user) {
-      console.log('User not found or password is invalid');
       throw new UnauthorizedException();
     }
 
